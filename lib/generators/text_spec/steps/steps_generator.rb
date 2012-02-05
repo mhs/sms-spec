8ddf96af3d4c11230d2b981f5464c8a4fb8aecc7
Chris Rittersdorf <manlycode@gmail.com>
2012-02-05 13:27:07 -0500

    Fixed bug where the generator template was missing.

--- lib/generators/text_spec/steps/steps_generator.rb
+++ lib/generators/text_spec/steps/steps_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators'
 module TextSpec
   class StepsGenerator < Rails::Generators::Base
     def generate
-      copy_file 'text_steps.rb', 'features/step_definitions/text_steps.rb'
+      copy_file 'text_message_steps.rb', 'features/step_definitions/text_message_steps.rb'
     end
 
     def self.source_root
--- lib/text_spec/version.rb
+++ lib/text_spec/version.rb
@@ -1,3 +1,3 @@
 module TextSpec
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
