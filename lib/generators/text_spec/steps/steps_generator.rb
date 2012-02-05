ac6f20e48a64c3f8e29a0fde664b1445dc0e6332
Chris Rittersdorf <manlycode@gmail.com>
2012-02-05 13:04:35 -0500

    Added Rails3 generators.

---
+++ lib/generators/text_spec/steps/steps_generator.rb
@@ -0,0 +1,15 @@
+# This generator adds email steps to the step definitions directory
+require 'rails/generators'
+
+module TextSpec
+  class StepsGenerator < Rails::Generators::Base
+    def generate
+      copy_file 'text_steps.rb', 'features/step_definitions/text_steps.rb'
+    end
+
+    def self.source_root
+      File.join(File.dirname(__FILE__), 'templates')
+    end
+  end
+end
+
