b16295f0a169d8975ca7c49a46dbd242517fc3ea
Chris Rittersdorf <manlycode@gmail.com>
2012-03-28 23:07:15 -0400

    Fixed another place where the cucumber step generator was broken.

--- lib/generators/sms_spec/steps/steps_generator.rb
+++ lib/generators/sms_spec/steps/steps_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators'
 module SmsSpec
   class StepsGenerator < Rails::Generators::Base
     def generate
-      copy_file 'text_message_steps.rb', 'features/step_definitions/text_message_steps.rb'
+      yopy_file 'sms_steps.rb', 'features/step_definitions/sms_steps.rb'
     end
 
     def self.source_root
