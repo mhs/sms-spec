c6ef05f8492b4a76fb4086ae5237fe3bef1ba31f
Chris Rittersdorf <manlycode@gmail.com>
2012-03-28 23:10:44 -0400

    Fixed an typo "yopy" => "copy"

--- lib/generators/sms_spec/steps/steps_generator.rb
+++ lib/generators/sms_spec/steps/steps_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators'
 module SmsSpec
   class StepsGenerator < Rails::Generators::Base
     def generate
-      yopy_file 'sms_steps.rb', 'features/step_definitions/sms_steps.rb'
+      copy_file 'sms_steps.rb', 'features/step_definitions/sms_steps.rb'
     end
 
     def self.source_root
--- lib/sms_spec/version.rb
+++ lib/sms_spec/version.rb
@@ -1,3 +1,3 @@
 module SmsSpec
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
