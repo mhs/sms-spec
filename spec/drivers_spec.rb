d93fd847387f5fabc0c03c5363bf8af66ea2cf2a
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2012-10-01 12:05:57 -0500

    * Updated read-me to reference proper gem name * patched account class to handle sid method * bumped version

--- spec/drivers_spec.rb
+++ spec/drivers_spec.rb
@@ -27,5 +27,23 @@ describe SmsSpec do
       open_last_text_message_for("+16105557069")
       current_text_message.should_not be_nil
     end
+
+    it "allows for sid method calls on the account object" do
+      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      @client.account.sms.messages.create(
+        :from => '+14159341234',
+        :to => '+16105557069',
+        :body => 'Hey there!'
+      )
+
+      open_last_text_message_for("+16105557069")
+      current_text_message.should_not be_nil
+      @client.account.should respond_to(:sid)
+      @client.account.sid.should be(account_sid)
+    end
+
   end
 end
