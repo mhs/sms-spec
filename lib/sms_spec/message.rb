4b3460da563a1ddcccc4b357aeb4932d8eec6c61
trg <me@tomgraft.com>
2013-06-14 14:00:25 -0400

    from number is tracked

--- lib/sms_spec/message.rb
+++ lib/sms_spec/message.rb
@@ -2,12 +2,14 @@ module SmsSpec
   class Message
     attr_accessor :number
     attr_accessor :body
+    attr_accessor :from
 
     include SmsSpec::Util
 
     def initialize(opts={})
       @number = sanitize opts[:number]
       @body = opts[:body]
+      @from = opts[:from]
     end
   end
 end
--- spec/drivers_spec.rb
+++ spec/drivers_spec.rb
@@ -45,5 +45,20 @@ describe SmsSpec do
       @client.account.sid.should be(account_sid)
     end
 
+    it "records the from number for a message" do
+      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      @client.account.sms.messages.create(
+        :from => '+14159341234',
+        :to => '+16105557069',
+        :body => 'Hey there!'
+      )
+
+      open_last_text_message_for("+16105557069")
+      current_text_message.from.should eq('+14159341234')
+    end
+
   end
 end
