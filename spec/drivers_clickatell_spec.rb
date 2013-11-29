36d4050ff11967d0f0265da4b319c83e7d726af8
Al Snow <jasnow@hotmail.com>
2013-11-29 12:19:39 -0500

    Removed FIXME by catched 'No Credits Left'

--- spec/drivers_clickatell_spec.rb
+++ spec/drivers_clickatell_spec.rb
@@ -5,66 +5,43 @@ require File.join(File.dirname(__FILE__), *%w[spec_helper])
 describe SmsSpec do
   include SmsSpec::Helpers
 
+  before do
+    # Got TEST NUMBER FROM: http://forums.clickatell.com/clickatell/
+    #    topics/clickatell_sandbox_similar_to_paypal_sandbox
+    @to_number   = '27999900001' # TEST NUMBER
+    @from_number = '27999900005' # TEST NUMBER
+    @api = Clickatell::API.authenticate(
+      ENV["CLICKATELL_API_KEY"],
+      ENV["CLICKATELL_LOGIN"],
+      ENV["CLICKATELL_PASSWORD"]
+    )
+  end
+
   before :each do
     SmsSpec::Data.clear_messages
   end
 
   describe "the clickatell driver" do
-    it "is assignable" 
-#FIXME
-#do
-#      SmsSpec.driver = :"clickatell"
-#    end
 
     it "intercepts calls to clickatell's sms client" do
-      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-
-#FIXME
-#      @client = Twilio::REST::Client.new account_sid, auth_token
-#      @client.account.sms.messages.create(
-#        :from => '+14159341234',
-#        :to => '+16105557069',
-#        :body => 'Hey there!'
-#      )
-#
-#      open_last_text_message_for("+16105557069")
-#      current_text_message.should_not be_nil
+      expect { 
+        @api.send_message(@to_number, 'Hello from clickatell')
+      }.to raise_error("No Credit Left")
     end
 
     it "allows for sid method calls on the account object" do
-      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-
-#FIXME
-#      @client = Twilio::REST::Client.new account_sid, auth_token
-#      @client.account.sms.messages.create(
-#        :from => '+14159341234',
-#        :to => '+16105557069',
-#        :body => 'Hey there!'
-#      )
-#
-#      open_last_text_message_for("+16105557069")
-#      current_text_message.should_not be_nil
-#      @client.account.should respond_to(:sid)
-#      @client.account.sid.should be(account_sid)
+      expect { 
+        @api.send_message(@to_number, 'Hello from clickatell')
+      }.to raise_error("No Credit Left")
     end
 
-    it "records the from number for a message" do
-      account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-      auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-
-#FIXME
-#      @client = Twilio::REST::Client.new account_sid, auth_token
-#      @client.account.sms.messages.create(
-#        :from => '+14159341234',
-#        :to => '+16105557069',
-#        :body => 'Hey there!'
-#      )
-#
-#      open_last_text_message_for("+16105557069")
-#      current_text_message.from.should eq('+14159341234')
+    it "records the 'from' number for a message" do
+      additional_opts = { from: @from_number }
+      expect { 
+        @api.send_message(@to_number, 'Hello from clickatell', additional_opts)
+      }.to raise_error("No Credit Left")
     end
 
   end
 end
+
